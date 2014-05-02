620fe44de977000bea3101d406c55ce0f24e042c
Jeremy <jjones9527@gmail.com>
2014-05-01 21:23:25 -0400

    Initial Commit

---
+++ myStory_02/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  myStory_02
+//
+//  Created by Jeremy Jones on 2014-05-01.
+//  Copyright (c) 2014 Jeremy Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ myStory_02/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myStory_02
+//
+//  Created by Jeremy Jones on 2014-05-01.
+//  Copyright (c) 2014 Jeremy Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
